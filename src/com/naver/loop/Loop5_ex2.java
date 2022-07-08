8c5d2eedfb1865276819ec3e1323cffd83f4b866
kimjinkyu <kjg08070@naver.com>
2022-07-08 17:42:30 +0900

    LoopEx5_Final

--- src/com/naver/loop/Loop5_ex2.java
+++ src/com/naver/loop/Loop5_ex2.java
@@ -21,58 +21,95 @@ public class Loop5_ex2
 		// -> 내가 내는 것 설정
 		// 0 - 가위, 1 - 바위, 2 - 보
 		// 내가 이길때까지 계속 실행
-		boolean ch = true;
-		while (ch)
+
+		// ------------ 강사님 ------------
+		boolean check = true;
+
+		while (check)
 		{
-			System.out.println("1. 가위, 2. 바위, 3. 보 (이길때까지 실행)");
-			int num = random.nextInt(4);
-			int th = sc.nextInt();
-			if (th == 1)
+			System.out.println("0. 가위, 1. 바위, 2. 보");
+			int aa = sc.nextInt();
+			int com = random.nextInt(3);
+			System.out.println("You: " + aa + " vs com: " + com);
+			
+			if (com == 0 && aa == 2)  // 0-2(가위>보) -> com win
 			{
-				System.out.println("가위");
+				aa = -1;
 			}
-			else if (th == 2)
+			if (com == 2 && aa == 0) // 2-0(보>가위) -> aa win
 			{
-				System.out.println("바위");
+				com = -1;
 			}
-			else if (th == 3)
+
+			if (com < aa)
 			{
-				System.out.println("보");
-			}
-			// -------------------------------------------------
-			// 승패 지정
-			System.out.println("사용자: " + th + " : " + "컴퓨터: " + num);
-			if (th == 1 && num == 2) // 가위 바위
-			{
-				System.out.println("---졌습니다.---");
-			}
-			else if (th == 1 && num == 3) // 가위 보
-			{
-				System.out.println("---이겼습니다!---");
+				System.out.println("You win");
 				break;
 			}
-			else if (th == 2 && num == 1) // 바위 가위
+			else if (com == aa)
 			{
-				System.out.println("---이겼습니다!---");
-				break;
+				System.out.println("Even");
 			}
-			else if (th == 2 && num == 3) // 바위 보
+			else
 			{
-				System.out.println("---졌습니다.---");
-			}
-			else if (th == 3 && num == 2) // 보 바위
-			{
-				System.out.println("---이겼습니다!---");
-				break;
-			}
-			else if (th == 2 && num == 0) // 보 가위
-			{
-				System.out.println("---졌습니다.---");
-			}
-			else if (th == num)
-			{
-				System.out.println("---비겼습니다.---");
+				System.out.println("Com win");
 			}
 		}
+
+		// ------------ 내가 한거 ------------
+
+		// boolean ch = true;
+		// while (ch)
+		// {
+		// System.out.println("1. 가위, 2. 바위, 3. 보 (이길때까지 실행)");
+		// int num = random.nextInt(4);
+		// int th = sc.nextInt();
+		// if (th == 1)
+		// {
+		// System.out.println("가위");
+		// }
+		// else if (th == 2)
+		// {
+		// System.out.println("바위");
+		// }
+		// else if (th == 3)
+		// {
+		// System.out.println("보");
+		// }
+		// // -------------------------------------------------
+		// // 승패 지정
+		// System.out.println("사용자: " + th + " : " + "컴퓨터: " + num);
+		// if (th == 1 && num == 2) // 가위 바위
+		// {
+		// System.out.println("---졌습니다.---");
+		// }
+		// else if (th == 1 && num == 3) // 가위 보
+		// {
+		// System.out.println("---이겼습니다!---");
+		// break;
+		// }
+		// else if (th == 2 && num == 1) // 바위 가위
+		// {
+		// System.out.println("---이겼습니다!---");
+		// break;
+		// }
+		// else if (th == 2 && num == 3) // 바위 보
+		// {
+		// System.out.println("---졌습니다.---");
+		// }
+		// else if (th == 3 && num == 2) // 보 바위
+		// {
+		// System.out.println("---이겼습니다!---");
+		// break;
+		// }
+		// else if (th == 3 && num == 1) // 보 가위
+		// {
+		// System.out.println("---졌습니다.---");
+		// }
+		// else if (th == num)
+		// {
+		// System.out.println("---비겼습니다.---");
+		// }
+		// }
 	}
 }
